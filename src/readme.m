fb4972d5b4022b5bf9d7ded4b487e53eef932a0f
Josue Quito <josueqf.06@gmail.com>
2023-03-29 12:13:21 -0500

    commit inicial

---
+++ src/readme.m
@@ -0,0 +1,4 @@
+The windows kernel is an open source software project of fairly large scope. During the early years of
+the windows kernel maintenance (1991–2002), changes to the software were passed around as patches
+and archived files. In 2002, the windows kernel project began using a proprietary DVCS called
+BitKeeper
